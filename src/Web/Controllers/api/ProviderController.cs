3b24b041328670736e05e7cfd17fff07f8e44e35
Nelson Wells <nelsonwellswku@gmail.com>
2017-01-22 16:19:39 -0600

    Web api endpoint for getting a provider

--- src/Web/Controllers/api/ProviderController.cs
+++ src/Web/Controllers/api/ProviderController.cs
@@ -1,6 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using MediatR;
 using Octogami.ProviderDirectory.Application.Feature.CreateProvider;
+using Octogami.ProviderDirectory.Application.Feature.GetProvider;
 
 namespace Octogami.ProviderDirectory.Web.Controllers.api
 {
@@ -17,6 +19,13 @@ namespace Octogami.ProviderDirectory.Web.Controllers.api
 		public IHttpActionResult PostProvider(CreateProviderCommand command)
 		{
 			var response = _mediator.Send(command);
+			return Ok(response);
+		}
+
+		[Route("api/Providers/{providerId}")]
+		public IHttpActionResult GetProvider(Guid providerId)
+		{
+			var response = _mediator.Send(new GetProviderQuery {ProviderId = providerId});
 			return Ok(response);
 		}
 	}
