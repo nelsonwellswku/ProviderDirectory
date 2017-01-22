406c2043473ab629c760160c862fa831341e75de
Nelson Wells <nelsonwellswku@gmail.com>
2017-01-22 16:11:53 -0600

    Add get provider query, handler, and validator

---
+++ src/Application/Feature/GetProvider/GetProviderHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+using Marten;
+using MediatR;
+using Octogami.ProviderDirectory.Application.Domain;
+
+namespace Octogami.ProviderDirectory.Application.Feature.GetProvider
+{
+	public class GetProviderQuery : IRequest<GetProviderResponse>
+	{
+		public Guid ProviderId { get; set; }
+	}
+
+	public class GetProviderResponse
+	{
+		public Guid ProviderId { get; set; }
+		public string NPI { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	public class GetProviderValidator : AbstractValidator<GetProviderQuery>
+	{
+		private readonly IDocumentSession _session;
+
+		public GetProviderValidator(IDocumentSession session)
+		{
+			_session = session;
+			RuleFor(x => x.ProviderId).Must(Exist);
+		}
+
+		private bool Exist(Guid providerId)
+		{
+			return _session.Query<Provider>().Any(x => x.ProviderId == providerId);
+		}
+	}
+
+	public class GetProviderHandler : IRequestHandler<GetProviderQuery, GetProviderResponse>
+	{
+		private readonly IDocumentSession _session;
+
+		public GetProviderHandler(IDocumentSession session)
+		{
+			_session = session;
+		}
+
+		public GetProviderResponse Handle(GetProviderQuery message)
+		{
+			var provider = _session.Load<Provider>(message.ProviderId);
+			return new GetProviderResponse
+			{
+				ProviderId = provider.ProviderId,
+				NPI = provider.NPI,
+				FirstName = provider.FirstName,
+				LastName = provider.LastName
+			};
+		}
+	}
+}
---
+++ src/Tests/Integration/Integration/Feature/GetProviderTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using FluentAssertions;
+using FluentValidation;
+using Marten;
+using MediatR;
+using NUnit.Framework;
+using Octogami.ProviderDirectory.Application.Domain;
+using Octogami.ProviderDirectory.Application.Feature.CreateProvider;
+using Octogami.ProviderDirectory.Application.Feature.GetProvider;
+using Octogami.ProviderDirectory.Application.Pipeline;
+using Octogami.ProviderDirectory.Tests.Integration.TestSupport;
+using StructureMap;
+
+namespace Octogami.ProviderDirectory.Tests.Integration.Feature
+{
+	public class GetProviderTests
+	{
+		private IContainer _container;
+
+		[OneTimeSetUp]
+		public void SetUpFixture()
+		{
+			var container = TestContainerFactory.New();
+			var documentStore = container.GetInstance<IDocumentStore>();
+			documentStore.Advanced.Clean.DeleteDocumentsFor(typeof(Provider));
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			_container = TestContainerFactory.New();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			_container.Dispose();
+		}
+
+		[Test]
+		public void GetProvider_HappyPath()
+		{
+			// Arrange
+			var mediator = _container.GetInstance<IMediator>();
+			var providerId = mediator.Send(new CreateProviderCommand
+			{
+				NPI = "123",
+				FirstName = "Elijah",
+				LastName = "Smith"
+			}).ProviderId;
+
+			// Act
+			var result = mediator.Send(new GetProviderQuery {ProviderId = providerId});
+
+			// Assert
+			result.ProviderId.Should().Be(providerId);
+			result.NPI.Should().Be("123");
+			result.FirstName.Should().Be("Elijah");
+			result.LastName.Should().Be("Smith");
+		}
+
+		[Test]
+		public void GetProvider_ProviderNotFound()
+		{
+			// Arrange
+			var mediator = _container.GetInstance<IMediator>();
+
+			// Act
+			Action act = () => mediator.Send(new GetProviderQuery {ProviderId = Guid.NewGuid()});
+
+			// Assert
+			act.ShouldThrow<ValidationException>();
+		}
+	}
+}
