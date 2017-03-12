8241e83902c2a3c41285692f4954926e48251bbf
Nelson Wells <nelsonwellswku@gmail.com>
2017-03-11 18:16:43 -0600

    Just enough code to parse an NPI row into a flat object

---
+++ src/NpiDataProcessor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+
+namespace Octogami.ProviderDirectory.NpiDataProcessor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var npiFilePath = ConfigurationManager.AppSettings["npiFilePath"];
+			using (var fileStream = new FileStream(npiFilePath, FileMode.Open))
+			using (var fileReader = new StreamReader(fileStream))
+			using (var csvReader = new CsvReader(fileReader))
+			{
+				csvReader.Configuration.Delimiter = ",";
+				csvReader.Configuration.HasHeaderRecord = false;
+				var records = csvReader.GetRecords<NpiRow>().Take(10000);
+				var tenThousandRecords = records.ToList();
+			}
+		}
+	}
+}
