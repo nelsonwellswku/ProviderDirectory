0d1b680cf5a5c6b0ece7b8cf3732018678edbfaf
Nelson Wells <nelsonwellswku@gmail.com>
2017-04-13 21:17:35 -0500

    Create controller and handler to list all taxonomies

---
+++ src/Application/Feature/Common/TaxonomyResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octogami.ProviderDirectory.Application.Feature.Common
+{
+	public class TaxonomyResponse
+	{
+		public Guid TaxonomyId { get; set; }
+
+		public string TaxonomyCode { get; set; }
+
+		public string Grouping { get; set; }
+
+		public string Classification { get; set; }
+
+		public string Specialization { get; set; }
+
+		public string Definition { get; set; }
+
+		public string Notes { get; set; }
+	}
+}
---
+++ src/Web/Controllers/Api/TaxonomyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using MediatR;
+using Octogami.ProviderDirectory.Application.Feature.Common;
+using Octogami.ProviderDirectory.Application.Feature.GetTaxonomies;
+
+namespace Octogami.ProviderDirectory.Web.Controllers.api
+{
+	public class TaxonomyController : ApiController
+	{
+		private readonly IMediator _mediator;
+
+		public TaxonomyController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[Route("api/taxonomy")]
+		public IPaged<TaxonomyResponse> GetTaxonomies([FromUri]GetTaxonomiesQuery query)
+		{
+			return _mediator.Send(query ?? new GetTaxonomiesQuery());
+		}
+	}
+}
