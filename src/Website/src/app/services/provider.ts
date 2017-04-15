de90611626a87a01d503fc41d60a4adf27fffd2d
Nelson Wells <nelsonwellswku@gmail.com>
2017-04-14 22:34:38 -0500

    Display taxonomy info on provider detail page

--- src/Website/src/app/services/provider.ts
+++ src/Website/src/app/services/provider.ts
@@ -1,16 +1,19 @@
-import { State } from './state';
+import { Taxonomy } from "app/services/taxonomy";
+import { State } from "app/services/state";
 
 export class Provider {
     providerId?: string;
     npi: string;
     firstName: string;
     lastName: string;
+    primaryTaxonomy: Taxonomy;
     mailingAddress?: Address;
     practiceAddress?: Address;
 
     constructor() {
         this.mailingAddress = new Address();
         this.practiceAddress = new Address();
+        this.primaryTaxonomy = new Taxonomy();
     }
 }
 
